80022a382e1c966545c8532e8779f1726d7442e6
usalman <umit.salman@bil.omu.edu.tr>
2021-12-24 11:25:50 +0300

    create QuestionCard props and JSX

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,3 +1,35 @@
-const QuestionCard = () => <div>Question Card</div>
+interface IQuestionCard {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: boolean;
+  questionNr: number;
+  totalQuestions: number;
+}
 
-export default QuestionCard
+const QuestionCard: React.FC<IQuestionCard> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <>
+    <p>
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => {
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>;
+      })}
+    </div>
+  </>
+);
+
+export default QuestionCard;
